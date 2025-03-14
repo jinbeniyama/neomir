66d34bf9aa61a79b59b91f62f60dc9cb91588791
Jin Beniyama <beniyama@ioa.s.u-tokyo.ac.jp>
2025-03-14 19:07:10 +0100

    Fisrt commit

---
+++ src/plot_NEATM.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Plot NEATM results.
+"""
+from argparse import ArgumentParser as ap
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import matplotlib.ticker as ticker
+
+mycolor = [
+    "#AD002D",
+    "#1e50a2",
+    "#69821b",
+    "#f055f0",
+    "#afafb0",
+    "#0095b9",
+    "#89c3eb",
+    "#ec6800",
+    "cyan",
+    "gold",
+    "magenta"
+    ]
+
+
+if __name__ == "__main__":
+    parser = ap(description="Plot NEATM results for NEOMIR.")
+    parser.add_argument(
+        "res", type=str, nargs="*",
+        help="Result of NEATM")
+    parser.add_argument(
+        "--out", type=str, default="NEATM_res.png",
+        help="Directory for output file")
+    parser.add_argument(
+        "--ymax", type=float, default=0,
+        help="Maxmimum y")
+    parser.add_argument(
+        "--outdir", type=str, default="plot",
+        help="Directory for output file")
+    args = parser.parse_args()
+
+    outdir = args.outdir
+    os.makedirs(outdir, exist_ok=True)
+
+    Gamma_values = [0, 50, 150, 300, 500, 1000]
+    
+    df_list = []
+    for res in args.res:
+        df = pd.read_csv(res, sep=" ")
+        df_list.append(df)
+
+    df = pd.concat(df_list)
+    # Diameter ratio
+    df["Dr"] = df["D_NEATM"]/df["D_true"]
+
+    # Plot
+    fig = plt.figure(figsize=(16, 4))
+    ax_a = fig.add_axes([0.10, 0.20, 0.25, 0.7])
+    ax_r = fig.add_axes([0.40, 0.20, 0.25, 0.7])
+    ax_d = fig.add_axes([0.70, 0.20, 0.25, 0.7])
+
+    ax_a.set_xlabel("Phase angle [deg]")
+    ax_r.set_xlabel("Heliocentric distance [au]")
+    ax_d.set_xlabel("NEOMIR-centric distance [au]")
+    ax_a.set_ylabel(r"$D_{NEATM}/D_{true}$")
+
+    for idx_TI, TI in enumerate(Gamma_values):
+        df_TI = df[df["TI"] == TI]
+        zorder = 100 - idx_TI
+        shift = idx_TI*1
+
+        ax_a.scatter(
+            df_TI["alpha"]+shift, df_TI["Dr"], label=f"{TI} tiu", color=mycolor[idx_TI], s=5, marker="o", fc="None", zorder=zorder)
+        ax_r.scatter(
+            df_TI["r"], df_TI["Dr"], label=f"{TI} tiu", color=mycolor[idx_TI], s=5, marker="o", fc="None", zorder=zorder)
+        ax_d.scatter(
+            df_TI["delta"], df_TI["Dr"], label=f"{TI} tiu", color=mycolor[idx_TI], s=5, marker="o", fc="None", zorder=zorder)
+
+    yticks = np.arange(0, 7, 1.0)
+    for ax in fig.axes:
+        ax.legend()
+        ax.yaxis.set_major_locator(ticker.FixedLocator(yticks))
+        if args.ymax:
+            ax.set_ylim([0, args.ymax])
+    out = os.path.join(outdir, args.out)
+    plt.savefig(out)
+    plt.close()
